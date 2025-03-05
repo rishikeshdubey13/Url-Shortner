3b525b137704dfbf437adbfe9928ef1d7d5ef133
Rishikesh Dubey <117086580+rishikeshdubey13@users.noreply.github.com>
2025-03-05 19:09:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://user1:Zompire%4017@localhost:5432/url_shortner"
+    SQLALCHEMY_DATABASE_URI = "postgresql://user1:password@localhost:5432/url_shortner"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_secret_key_here")  # Replace with a strong secret
 
